2307ace15d049c776c4096f6879159b402123f73
TetsuyaTobiichi <91623795+TetsuyaTobiichi@users.noreply.github.com>
2021-10-10 20:06:24 +0800

    программа сдана

---
+++ WPF Lab 2.2Tests/LogicTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WPF_Lab_2._2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Lab_2._2.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void DelDupTest()
+        {
+            string forTest = "aaa";
+            string message = Logic.DelDup(forTest);
+            Assert.AreEqual("aaa", message);
+        }
+
+        [TestMethod()]
+        public void FindSimilarLettersTest()
+        {
+            string FirstTestWord = "компьютер";
+            string SecondTestWord = "процессор";
+            string message = Logic.FindSimilarLetters(logic.DelDup(SecondTestWord), logic.DelDup(FirstTestWord));
+            Assert.AreEqual("yes yes yes no yes no ", message);
+        }
+    }
+}
